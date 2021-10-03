b4d66c5ce6baadd3acc37f47aae19830d4890612
Gourav Mishra <88934396+gouravmarch20@users.noreply.github.com>
2021-10-03 11:44:27 +0530

    initial api added

---
+++ src/assets/data.js
@@ -0,0 +1,77 @@
+const data = {
+  apsDk: [
+    //   basic info
+    {
+      name: 'Delhi Technological University',
+      location: 'Ridge road , dhaula kuan ',
+      establishedYear: '1953',
+      affilatedBoard: 'C.B.S.E',
+      medium: 'English medium',
+      category: 'Public school',
+      nearestPlace: {
+        metroStation: '5km',
+        busStand: '3.2km',
+        atm: '100m'
+      }
+    },
+    // about managment
+    {
+      aboutManagement:
+        'The vision of Army Public School, Dhaula Kuan: To groom students into refined and disciplined global citizens, who can blend socially and culturally in a multi-faceted society',
+      principleName: ' Komal Sood'
+    },
+    // infastructure detail
+    {
+      outdoorSport:
+        'basketball court, football ground, handball ground, tennis court, volleyball court, hockey ground.',
+      indoorSport: 'Chess, Carrom, etc',
+      libary: '2',
+      computerLab: 'two computer Lab',
+      physicsLab: 1,
+      chemistryLab: 1,
+      biologyLab: 1,
+      totalFloor: '2 floor ',
+      canteen: 'Available ',
+      auidtorium: 'yes',
+      coCurricular: ''
+    },
+    // general question
+    {
+      timingOfSchool: {
+        summer: '7:30 AM To 1:40 PM',
+        Winter: '7:30 AM To 1:40 PM'
+      },
+      summerUniform: {
+        Girls:
+          '(a)skirt:grey , (b) shirt : white (c) shocks: white (d) shoes : black ',
+        Boys: '(a) Shorts: Grey, (b) shir : white'
+      },
+      winterUniform: {
+        Girl:
+          'is simply dummy text of the printing and typesetting industry. Lorem Ipsum has been the industry',
+        boy:
+          'ok. It has survived not only five centuries, but also the leap into el'
+      },
+      addmission: {
+        howToGet:
+          ' Require to fill the registration forms through offline mode in school office or registration form are available online.  date for applying: recommended to contact school official in last week of march or first week of April .  '
+      }
+    },
+    //contact detail
+    {
+      website: 'www.apsdk.com',
+      emailId: 'info@apsdk.com',
+      address: 'ridge road '
+    }
+  ],
+
+  sasssDelhi: [
+    {
+      name: 'St Andrews Scotts Senior Sec School ',
+      palace: 'rajasthan',
+      rating: 'rating : 3',
+
+      image: 'xxxxxxxxxxxxxxxxx'
+    }
+  ]
+}
